e2a2750790bbd1594a99aeaf9d974ae00d64edd8
MatveyErshov55 <165894509+MatveyErshov55@users.noreply.github.com>
2024-04-03 12:22:59 +0300

    Add files via upload

---
+++ Pr-1(03.04.2024).py
@@ -0,0 +1,12 @@
+x = int(input())
+a = 0
+b = 0
+#hhkkkjkkjkj
+a1 = 0
+a2 = 0
+while x!=-1000:
+    if x >=0:
+        a+=x
+        a1+=1
+    else:
+print("отрицательных фрифметическое средне:",b/)
